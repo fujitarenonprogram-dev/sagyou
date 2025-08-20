b83346f657b5fdd76ec6bcaedf123a4914793c9a
fujita.renon.program <fujita.renon.program@gmail.com>
2025-08-20 12:43:07 +0900

    ファイルアップロード機能を追加

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("コードを追加");
+var uploadFile = function () {
+    console.log("ボタンがクリックされました");
+    var inputFile = document.getElementById("movieFile");
+    if (!inputFile) {
+        alert("アップロードに失敗しました");
+        return;
+    }
+    var file = inputFile.files ? inputFile.files[0] : undefined;
+    if (!file) {
+        alert("ファイルを選んでください");
+        return;
+    }
+    console.log("選択されたファイル:", file.name);
+};
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("コードを追加")
+const uploadFile = () => {
+    console.log("ボタンがクリックされました");
+    const inputFile = document.getElementById("movieFile") as HTMLInputElement|null;
+        if (!inputFile) {
+            alert("アップロードに失敗しました");
+            return;
+            }
+    const file: File | undefined = inputFile.files ? inputFile.files[0] : undefined;
+
+    if (!file) {
+        alert("ファイルを選んでください");
+        return;
+    }
+
+    console.log("選択されたファイル:", file.name);
+} 
